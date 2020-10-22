77c9454785d8db6735d8e2f0c8194f77d191bd2f
Natalia <safary-1994@mail.ru>
2020-10-22 22:32:44 +0300

    fix: division of doubleItems

--- src/index.js
+++ src/index.js
@@ -1,42 +1,46 @@
-const items = ['zero', 'one', 'two', 'three',
-'four', 'five', 'six', 'seven', 'eight', 'nine'];
-
-const doubleItems = ['ten', 'eleven', 'twelve', 'thirteen',
+const items = ['', 'one', 'two', 'three',
+'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen',
  'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
 
-
-const decadeItems = ['', '', 'twenty', 'thirty',
+const doubleItems = ['', '', 'twenty', 'thirty',
  'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
 module.exports = function toReadable (num) {
-if  (num < 10) {
-    return items[num];
-} else if(num>=10 && num < 20) {
-    return doubleItems[num - 10];
-} else if (num>= 20 && num < 100) {
-    return decadeNum(num);
-} else if(num >= 100 && num < 1000) {
-    return hundredNum(num);
- }
-}
-
- function decadeNum(num) {
-     num = num.toString().split('');
-     if(num[1] === 0) {
-         return decadeItems[num[0]];
-     } else {
-         return decadeItems[num[0]] + ' ' + items[num[1]];
+    if  (num === 0) {
+        return 'zero';
+    } else if(num < 20) {
+        return items[num];
+    } else if (num>= 20 && num < 100) {
+        return decadeNum(num);
+    } else if(num >= 100 && num < 1000) {
+        return hundredNum(num);
      }
- }
-
-
- function hundredNum(num) {
-     num = num.toString().split('');
-     if(num[1] === 0 && num[2] === 0) {
-         return items[num[0]] + ' hundred';
-     } else if (num[1] === 0 && num[2] !== 0) {
-         return items[num[0]] + ' hundred' + items[num[2]];
-     } else if (num[1] !== 0 && num[2] === 0) {
-         return items[num[0]] + ' hundred' + decadeItems[num[1]];
+    }
+    
+    function decadeNum(num) {
+         num = num.toString().split('');
+         if(num[1] == 0) {
+             return doubleItems[num[0]];
+         } else {
+            return doubleItems[num[0]] + ' ' + items[num[1]];
+         }
      }
- };
+    
+
+function hundredNum(num) {
+    num = num.toString().split('');
+    if(num[1] == 0 && num[2] == 0) {
+        return items[num[0]] + ' hundred';
+     } else if(num[1] == 0 && num[2] != 0) {
+        return items[num[0]] + ' hundred' + items[num[2]];
+    // } else if (num[1] !== 0 && num[2] === 0) {
+    //     return items[num[0]] +  ' hundred' + doubleItems[num[1]] + items[num[2]];
+     }
+};
+
+
+
+
+
+
+ 
